a0db47096fc520288dee4f70c3ba39f2e5023395
Lakshman <lakshmanram2259@gmail.com>
2023-01-27 19:40:56 +0530

    all the files like gitignore, packages.json, tailwind are added

---
+++ src/State/Reducer.jsx
@@ -0,0 +1,15 @@
+export const initialState = {
+  tv: [],
+};
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TV": {
+      return {
+        tv: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
